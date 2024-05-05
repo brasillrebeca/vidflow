9b883ae0dbfca883a5dfca34d1212c9c43588e13
brasillrebeca <rebecabrasil9450@gmail.com>
2024-05-05 19:01:54 -0300

    aula concluída

--- projeto-vidflow-main/script.js
+++ projeto-vidflow-main/script.js
@@ -41,3 +41,45 @@ async function buscarEMostrarVideos() {
 }
 
 buscarEMostrarVideos();
+
+const barraDePesquisa = document.querySelector("pesquisar__input");
+
+barraDePesquisa.addEventListener("input", filtrarPesquisa);
+function filtrarPesquisa() {
+    const videos = document.querySelectorAll(".videos__itm");
+
+    if(barraDePesquisa.value != "") {
+        for(let video of videos) {
+            let titulo = video.querySelector(".titulo-video").textContent.toLowerCase();
+            let valorFiltro = barraDePesquisa.value.toLowerCase();
+             if(!titulo.includes(valorFiltro)) {
+                video.computedStyleMap.display = "none";
+             } else {
+                video.computedStyleMap.display = "block";
+             }
+        }
+    } else {
+        video.style.display = "block";
+    }
+}
+
+const botaoCategoria = document.querySelectorAll(".superior__item");
+
+botaoCategoria.forEach((botao) => {
+    let nomeCategoria = botao.getAttribute("name");
+    botao.addEventListener("click", () => filtrarPorCategoria(nomeCategoria));
+});
+
+function filtrarPorCategoria(filtro) {
+    const videos = document.querySelectorAll(".videos__item");
+    for(let video of videos){
+        let categoria = video.querySelector(".categoria").textContent.toLocaleLowerCase();
+        let valorFiltro = filtro.toLocaleLowerCase();
+
+        if(!categoria.includes(valorFiltro) && valorFiltro != 'tudo') {
+            video.style.display = 'none';
+        } else {
+            video.style.display = 'block';
+        }
+    }
+}
